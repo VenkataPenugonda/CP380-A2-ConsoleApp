da7ad29ae6b3678442d4f5ec25efe67e62028ae4
Venkata Penugonda <penugondapavan01@gmail.com>
2021-08-18 13:39:33 -0400

    first commit

--- BreadmakerReport/Program.cs
+++ BreadmakerReport/Program.cs
@@ -3,29 +3,37 @@ using System;
 using System.Linq;
 using RatingAdjustment.Services;
 using BreadmakerReport.Models;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Threading;
+using System.ComponentModel;
 
 namespace BreadmakerReport
 {
     class Program
     {
         static string dbfile = @".\data\breadmakers.db";
-        static RatingAdjustmentService ratingAdjustmentService = new RatingAdjustmentService();
+        static RatingAdjustmentService rasObject = new RatingAdjustmentService();
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Bread World");
+            Console.WriteLine("Welcome to Bread World");            
             var BreadmakerDb = new BreadMakerSqliteContext(dbfile);
-            var BMList = BreadmakerDb.Breadmakers
-                // TODO: add LINQ logic ...
-                //       ...
+            var BMList = BreadmakerDb.Breadmakers                
+                .Select(bm => new { review = bm.Reviews.Count
+                                    , avg = Math.Round(bm.Reviews.Average(r => r.stars), 2)
+                                    , adjust = Math.Round(rasObject.Adjust(bm.Reviews.Average(r => r.stars) , bm.Reviews.Count), 2)
+                                    , title = bm.title
+                })          
+                .AsEnumerable()
+                .OrderByDescending(bm => bm.adjust)
                 .ToList();
 
-            Console.WriteLine("[#]  Reviews Average  Adjust    Description");
+            Console.WriteLine("\n-------------------------------------------\n[#]  Reviews Average  Adjust    Description\n-------------------------------------------");
             for (var j = 0; j < 3; j++)
             {
                 var i = BMList[j];
-                // TODO: add output
-                // Console.WriteLine( ... );
+                // TODO: add output                
+                Console.WriteLine($"[{j+1}] {i.review,8} {i.avg, -2:F2} {i.adjust, 8:F2}      {i.title}");
             }
         }
     }
--- RatingAdjustmentService/RatingAdjustmentService.cs
+++ RatingAdjustmentService/RatingAdjustmentService.cs
@@ -7,6 +7,53 @@ namespace RatingAdjustment.Services
      */
     public class RatingAdjustmentService
     {
-        // TODO: Replace this file with the one from Part A!
+        const double MAX_STARS = 5.0;  // Likert scale
+        const double Z = 1.96; // 95% confidence interval
+
+        double _q;
+        double _percent_positive;
+
+        /** Percentage of positive reviews
+         * 
+         * In this case, that means X of 5 ==> percent positive
+         * 
+         * Returns: [0, 1]
+         */
+        void SetPercentPositive(double stars)
+        {
+            this._percent_positive = stars / RatingAdjustmentService.MAX_STARS;
+        }
+
+        /**
+         * Calculate "Q" given the formula in the problem statement
+         */
+        void SetQ(double number_of_ratings)
+        {
+            double left_half = this._percent_positive * (1 - this._percent_positive);
+            double power = Math.Pow(RatingAdjustmentService.Z, 2);
+            double multiply = 4 * number_of_ratings;
+            double ans1 = (left_half + (power / multiply));
+            double final_result = ans1 / number_of_ratings;
+            this._q = RatingAdjustmentService.Z * Math.Sqrt(final_result);
+        }
+
+        /** Adjusted lower bound
+         * 
+         * Lower bound of the confidence interval around the star rating.
+         * 
+         * Returns: a double, up to 5
+         */
+        public double Adjust(double stars, double number_of_ratings) {
+            
+            this.SetPercentPositive(stars);
+            this.SetQ(number_of_ratings);
+
+            double power = Math.Pow(RatingAdjustmentService.Z, 2);
+            double left_half = (this._percent_positive + ( power / (2 * number_of_ratings)) - this._q);
+            double right_half = (1 + (power / number_of_ratings));
+            double final_result = left_half / right_half * RatingAdjustmentService.MAX_STARS;
+
+            return final_result;
+        }
     }
 }
